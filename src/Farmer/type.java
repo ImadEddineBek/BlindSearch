b3ddf2d1c7bb1e7c0bb1118e26abea07c7071245
ImadEddineBek <bek34046629@yahoo.com>
2017-10-15 18:52:55 +0100

    First Implementation with Bidon

--- src/Farmer/type.java
+++
@@ -1,5 +0,0 @@
-package Farmer;
-
-public enum type {
-    loupChevre , loupBaton , loupFeu , loupChoux ,loup , chevre , baton ,feu , choux ,chevreBaton , ChevreFeu , ChevreChoux , BbatonFeu , BatonChoux ,FeuChoux
-}
